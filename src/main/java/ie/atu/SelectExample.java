0bdf444bf7a8ac9cbd9ad26d905608fbed786c15
JOHNROSSDOHERTY-STUD <g00423043@atu.ie>
2025-03-10 12:04:10 +0000

    a

--- src/main/java/ie/atu/SelectExample.java
+++ src/main/java/ie/atu/SelectExample.java
@@ -1,7 +1,12 @@
 package ie.atu;
 
+import java.sql.*;
 public class SelectExample {
-    String url = "jdbc:mysql://localhost:3306/exampledatabase";
+    String url = "jdbc:mysql://localhost:3306/newdatabase";
     String username = "root";
     String password = "password";
+
+    String selectSQL = "SELECT u.username, u.password, e.email " +
+            "FROM users u " +
+            "JOIN emails e ON u.id = e.user_id";
 }
